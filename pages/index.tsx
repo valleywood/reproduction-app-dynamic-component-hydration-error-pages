7547cfe509bc2f21f697535778b5b31539a8aa35
Per Daleskog <per.daleskog@mobilicon.se>
2023-09-20 17:38:09 +0200

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+/** Add your relevant code here for the issue to reproduce */
+import StartPage from '@valleywood/reproduction-app-dynamic-component-hydration-error-package/index';
+export default function Home() {
+  console.log('StartPage');
+  return <StartPage />;
+}
